ba61c8c0c0f0b5e99299a0864ac16d0044e4d909
openupmta <openupmta99@gmail.com>
2019-09-04 10:59:34 +0700

    index

--- resources/views/admins/layouts/footer.blade.php
+++ resources/views/admins/layouts/footer.blade.php
@@ -1,7 +1,6 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-        <b>Version</b> 2.4.13
+        <b>Version</b> 1.0
     </div>
-    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE</a>.</strong> All rights
-    reserved.
+    <strong>Copyright &copy; 2018-2019</strong>
 </footer>
